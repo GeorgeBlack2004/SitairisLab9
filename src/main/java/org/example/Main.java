aec4ffde54a5b3282b084763ad5212f0c83f65a2
Ziraiel <georgechernovedu@gmail.com>
2024-12-07 00:19:56 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String finalLab = "EEEEEEEEEBABYYYYYY, IT'S FINAL LAB IN SEMESTR";
+        for (int i = 1; i <= finalLab.length(); i++) {
+            System.out.println(finalLab + " " + i + " vIlLy ViLlY");
+        }
+    }
+}
